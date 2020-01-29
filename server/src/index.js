0338102de7a2f1667cf7018fcbb80da1f9ee839c
Ephriam Henderson <hendersonephriamn@gmail.com>
2020-01-29 13:02:03 -0600

    Added multischema support.

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,34 @@ const { ApolloServer } = require('apollo-server')
 const { importSchema } = require('graphql-import')
 
 function buildSchema() {
-    return importSchema("src/graphql/db.graphql")
+    return new Promise((resolve, reject) => {
+        // Define each async import schema call.
+        const schemas = {
+            db: importSchema("src/graphql/db.graphql"),
+            app: importSchema("src/graphql/app.graphql")
+        }
+
+        let finalSchema = ""
+        const keys = Object.keys(schemas)
+
+        // Place each import schema promise into an array.
+        const schemaPromises = keys.map(key => schemas[key])
+        Promise.all(schemaPromises)
+            .then(schemaStrings => {
+                // Add each schema string to the final schema.
+                schemaStrings.forEach(schema => {
+                    finalSchema += '\n' + schema
+                })
+                // console.log(finalSchema)
+                resolve(finalSchema)
+            })
+    })
+
 }
 
 buildSchema()
 .then(typeDefs => {
+    // console.log(typeDefs)
     const server = new ApolloServer({typeDefs})
     server.listen({port:4000})
         .then(({ url }) => {
